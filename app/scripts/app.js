af6cdcb2318a1d199b78e2346ba605cf17d08739
Brookelyn <brooke.balza@gmail.com>
2016-03-31 16:42:25 +0000

    Ui-router working and data pulling in

---
+++ app/scripts/app.js
@@ -0,0 +1,48 @@
+var app = angular.module('app', ['ui.router']);
+
+// Set up
+app.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+
+  $stateProvider.state('home', {
+    url: '/',
+    controller: 'homeController',
+    templateUrl: 'templates/home.html'
+  });
+}]);
+
+// Pull in the data
+app.factory('newsFeed', function($http){
+  var json;
+
+  function getJSON(callback) {
+    // var url = '';
+    return $http.get('https://s3-eu-west-1.amazonaws.com/streetlife-coding-challenge/newsfeed.json')
+    .success(function(data) {
+      json = data;
+      callback(data);
+    })
+    .error(function(data) {
+      console.log("Looks like something's gone wrong...")
+    });
+  }
+
+  return {
+    getNewsfeed: function(callback) {
+      if (json) {
+        return json;
+      } else {
+        getJSON(callback);
+      }
+    }
+  };
+})
+
+// Homepage controller
+app.controller('homeController', function($scope, newsFeed) {
+
+  newsFeed.getNewsfeed(function(json) {
+    $scope.feed = json.messages;
+    console.log(json);
+  })
+});
