d0c0e597b81169d9056edbaee2e870da0a657ca0
Brookelyn <brooke.balza@gmail.com>
2016-04-02 11:43:19 +0000

    Started setting up Comment feed factory and template

--- dist/js/app.js
+++ dist/js/app.js
@@ -9,14 +9,20 @@ app.config(['$stateProvider', '$locationProvider', function($stateProvider, $loc
     controller: 'homeController',
     templateUrl: 'templates/home.html'
   });
+
+  $stateProvider.state('comments', {
+    url: '/comments',
+    controller: 'commentsController',
+    templateUrl: 'templates/comments.html'
+  });
+
 }]);
 
-// Pull in the data
+// Pull in newsfeed data
 app.factory('newsFeed', function($http){
   var json;
 
   function getJSON(callback) {
-    // var url = '';
     return $http.get('https://s3-eu-west-1.amazonaws.com/streetlife-coding-challenge/newsfeed.json')
     .success(function(data) {
       json = data;
@@ -38,20 +44,70 @@ app.factory('newsFeed', function($http){
   };
 })
 
+// Pull in comment feed data
+app.factory('commentFeed', function($http){
+  var json;
+
+  function getJSON(callback) {
+    return $http.get('https://s3-eu-west-1.amazonaws.com/streetlife-coding-challenge/comments.json')
+    .success(function(data) {
+      json = data;
+      callback(data);
+    })
+    .error(function(data) {
+      console.log("Looks like something's gone wrong...")
+    });
+  }
+
+  return {
+    getCommentFeed: function(callback) {
+      if (json) {
+        return json;
+      } else {
+        getJSON(callback);
+      }
+    }
+  };
+})
+
 // Homepage controller
 app.controller('homeController', function($scope, newsFeed) {
 
   newsFeed.getNewsfeed(function(json) {
     $scope.feed = json.messages;
+  });
+})
+  .directive('messageBody', function() {
+    return {
+        template: '<p class="message-body" ng-bind-html="\'{{ newsitem.body }}\'"></p>'
+      };
   })
-})
-.directive('messageBody', function() {
-  return {
-      template: '<p class="message-body" ng-bind-html="\'{{ newsitem.body }}\'"></p>'
+  .directive('commentBody', function() {
+    return {
+      template: '<p class="comment-body" ng-bind-html="\'{{ comment.body }}\'"></p>'
     };
-})
-.directive('commentBody', function() {
-  return {
-    template: '<p class="comment-body" ng-bind-html="\'{{ comment.body }}\'"></p>'
+});
+
+// Comment feed controller
+app.controller('commentsController', function($scope, commentFeed) {
+  commentFeed.getCommentFeed(function(json) {
+    $scope.feed = json.comments;
+  });
+});
+
+// Updated at filter
+
+app.filter('pubAt', function() {
+  return function(posted_at){
+    var today = new Date();
+    var posted = posted_at;
+
+    var time = posted.substring(posted.lastIndexOf("T")+1,posted.lastIndexOf("."));
+
+
+
+     return today;
   };
+
+ 
 });
